7e8ab240b9e0768ff439c35b948a3b0b2f0c5b63
souravbaranwal <sourav.baranwal@gmail.com>
2019-06-20 16:34:00 +0530

    completed battle component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import "./App.scss";
 import Popular from "./Popular";
-import { Route, BrowserRouter as Router } from "react-router-dom";
-import Nav from './Nav';
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Nav from "./Nav";
+import Home from "./Home";
+import Battle from "./Battle";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Nav/>
-        <Route path='/popular' component={Popular}/>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/popular" component={Popular} />
+          <Route exact path="/battle" component={Battle} />
+          <Route render = {() => <p>Not found</p>}/>
+        </Switch>
       </div>
     </Router>
   );
